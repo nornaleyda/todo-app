9bf1ffb8b1b5d638cd1dbb16c3369edbf54dfb66
nornaleyda <naleyda2408rosli@gmail.com>
2023-03-29 17:20:49 +0800

    hello

---
+++ script/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AppWrapper from "./Components/Common/AppWrapper";
+import Layout from "./Components/Common/Layout";
+
+const App = () => {
+    return (
+        <div>
+            <AppWrapper>
+                <Layout>
+                    <div>test</div>
+                </Layout>
+            </AppWrapper>
+        </div>
+    )
+}
+
+export default App;
