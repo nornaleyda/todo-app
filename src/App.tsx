75236e04caa0e9c8cb95d4b9b5029c087ba17ff9
nornaleyda <naleyda2408rosli@gmail.com>
2023-03-30 09:50:45 +0800

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppWrapper from './AppWrapper';
+import Layout from './layout';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <AppWrapper>
+              <Layout>
+                  <div>test</div>
+              </Layout>
+          </AppWrapper>
+      </div>
+  )
 }
-
 export default App;
